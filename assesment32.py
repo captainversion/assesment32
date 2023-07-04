63e70f07fcfff2954d9c6dd1e029341ec72ed991
captainversion <136670723+captainversion@users.noreply.github.com>
2023-07-04 11:42:49 +0530

    assesment32.py

--- assesment32.py
+++ assesment32.py
@@ -2,6 +2,6 @@ def reverse(string):
     string = list(string)
     string.reverse()
     return "".join(string)
-s = "Ganesh"
+s = input("enter the string:")
 print("The string  is : ", s)
 print("reverse string is:",reverse(s))
